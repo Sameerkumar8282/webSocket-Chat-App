742859c340e909b57ff3be9ad1bb01fc54b577bc
Sameer Kumar <sameerkumar1012.sk@gmail.com>
2024-07-15 11:40:51 +0530

    first cimmit

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,17 @@ const server = http.createServer(app);
 //convert http into the io server
 const io = new Server(server);
 
+//Socket.io
+io.on('connection',(socket)=>{
+//every socket has an id
+ socket.on('chatMessage',message =>{
+  console.log(message);
+  //broadcast to all the clients
+  io.emit('chatMessage',message);
+ })
+ console.log('A new user connected',socket.id);
+})
+
 app.use(express.static("/public"));
 app.use(express.static(path.join(__dirname, 'public')));
 
